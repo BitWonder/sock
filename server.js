1c18209562579e1719bd0449e321fc5fc54ca3e9
BitWonder <drake@morgancomputers.net>
2024-02-22 20:58:01 -0600

    more indepth check for body

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ function correct_password(username, password) {
 
 router.post("/login", async (context) => {
   const { request, response } = context;
-  if (!request.hasBody) {console.log("No Body!!!!!!!!!!!!!!!!!! Aughherhfiubsdfui")}
+  if (!request.hasBody) {console.log("No Body!!!!!!!!!!!!!!!!!! Aughherhfiubsdfui")} else {console.log("Has body :)")}
   const body = await request.body.value;
   const info = body;
 
