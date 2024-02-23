d92ca2545678a92131d5c2d7eda7991e43c8d68e
BitWonder <drake@morgancomputers.net>
2024-02-23 15:43:06 -0600

    get

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ function correct_password(username, password) {
   return false;
 }
 
-router.post("/login/:username", () => {
+router.get("/login/:username", () => {
   return new Response(`${username}`)
 })
 
