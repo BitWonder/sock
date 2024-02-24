6d1c4467851d9f0e92e7db638209d7384f8f135a
BitWonder <drake@morgancomputers.net>
2024-02-24 11:57:17 -0600

    see const

--- server.js
+++ server.js
@@ -92,6 +92,8 @@ router.get("/chat", async (ctx) => {
   const socket = await ctx.upgrade();
   const username = ctx.request.url.searchParams.get("username");
   const room = ctx.request.url.searchParams.get("room");
+  console.log("Username" + username)
+  console.log("Room: "+ room)
   socket.onopen = () => {
     let current_list = rooms.get(room)
     console.log(current_list);
