f12d4fa0b4706d20b482193fe0aafd8224bfb622
BitWonder <drake@morgancomputers.net>
2024-02-29 15:42:16 -0600

    added await

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ router.get("/new_room", async (ctx) => {
   const socket = await ctx.upgrade();
   socket.onmessage = async (message) => {
     let make = JSON.parse(message.data);
-    let x = database.get(["rooms"])
+    let x = await database.get(["rooms"])
     console.log(make);
     console.log(x)
     if (x.includes(make.room)) {
